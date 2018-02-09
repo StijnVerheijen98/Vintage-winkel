373fb1f9fd5a4f457faf84ee1e08495616b9bfd6
ZeroThe2nd <nightfireluna@gmail.com>
2018-02-09 10:34:50 +0100

    -Enabled Facades for easier access to functions -Added an example route config for the products

--- api/routes/web.php
+++ api/routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
+/** @var \Laravel\Lumen\Routing\Router $router */
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+// API Versioning, V1
+$router->group(['prefix' => 'v1', 'namespace' => 'V1'], function () use ($router) {
+    // /api/v1/
+    $router->group([['middleware' => 'auth']], function () use ($router) {
+        // Uses the auth middleware to handle authentication
+    });
+
+    $router->group(['prefix' => 'products'], function () use ($router) {
+        // /api/v1/products
+        // Get products
+        $router->get('', 'Products@all');
+        // Get one product
+        $router->get('{id}', 'Products@get');
+
+        $router->group(['middleware' => 'auth'], function () use ($router) {
+            // require authentication
+            // Add a product
+            $router->post('', 'Products@add');
+            // Update a product
+            $router->put('{id}', 'Product@update');
+            // Delete a product
+            $router->delete('{id}', 'Product@delete');
+        });
+    });
+});
+
